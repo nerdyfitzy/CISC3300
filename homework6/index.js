a7e1ac67c71e33f0d3bd84ca35841e56d21c5bdc
nerdyfitzy <65741665+nerdyfitzy@users.noreply.github.com>
2024-02-29 16:38:40 -0500

    json server doesnt work

--- homework6/index.js
+++ homework6/index.js
@@ -1,32 +1,30 @@
-import got from "got";
-import * as $ from "jquery";
-
 const getPokemonFromName = async (name) => {
   try {
-    const { body } = await got.get(
+    const res = await fetch(
       `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`
     );
     console.log("complete");
-    const { order, sprites } = JSON.parse(body);
+    const { order, sprites } = await res.json();
     const img = sprites.front_default;
-
     return { num: order, img };
   } catch (e) {
     console.log(e);
-
     return { num: null, img: null };
   }
 };
 
 $("#submit").on("click", async (e) => {
-  const name = $("#name").text();
-  console.log(name);
+  const name = $("#name").val();
+  console.log("submit", name);
 
   const { num, img } = await getPokemonFromName(name);
 
   if (num !== null && img !== null) {
     $("#img-wrap").html(`<img src="${img}" alt="">`);
-    $("#num").text(num);
+    $("#num").text(`Pokedex number ${num}`);
+  } else {
+    $("#img-wrap").html(`Name not found`);
+    $("#num").text(``);
   }
 });
-getPokemonFromName("ditto");
+// getPokemonFromName("ditto");
